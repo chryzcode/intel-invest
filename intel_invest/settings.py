85e7dc60bd8f2c3afae68831dbf5d6e7ea3ba4e9
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2021-10-23 22:00:01 +0100

    commit

--- intel_invest/settings.py
+++ intel_invest/settings.py
@@ -136,6 +136,9 @@ STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
+MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
