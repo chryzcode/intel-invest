052d98529bbe1292193c703b4a12663dcaa834bb
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2021-11-01 00:48:01 +0100

    push

--- intel_invest_app/models.py
+++ intel_invest_app/models.py
@@ -10,3 +10,13 @@ class User(AbstractUser):
     USERNAME_FIELD = 'email'
     
     REQUIRED_FIELDS = ['username']
+
+
+class Packages(models.Model):
+    package_name = models.CharField(max_length=250)
+    package_price = models.IntegerField()
+    package_description = models.TextField(null=True, blank=True)
+    investors = models.ManyToManyField(User, related_name='investors')
+
+    def __str__(self):
+        return self.package_name
