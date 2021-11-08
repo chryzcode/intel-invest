e0d147368c560ea0f16151f1205f3c5baed8034a
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2021-11-08 01:02:30 +0100

    commit

--- intel_invest/settings.py
+++ intel_invest/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['intel-invest-app.herokuapp.com/', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['intel-invest-app.herokuapp.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
