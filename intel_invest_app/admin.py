5efe4a3b68e1f34e15fc2c9ac70808f3a3ddd8cf
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2021-11-16 10:37:03 +0100

    push

--- intel_invest_app/admin.py
+++ intel_invest_app/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-# from .models import User, Packages, Cryptocurrency, Payment, UserWallet, ConfirmPayment
-# # # Register your models here.
+from .models import User, Packages, Cryptocurrency, Payment, UserWallet, ConfirmPayment
+# # Register your models here.
 
-# admin.site.register(User)
-# admin.site.register(Packages)
-# admin.site.register(Payment)
-# admin.site.register(Cryptocurrency)
-# admin.site.register(UserWallet)
-# admin.site.register(ConfirmPayment)
+admin.site.register(User)
+admin.site.register(Packages)
+admin.site.register(Payment)
+admin.site.register(Cryptocurrency)
+admin.site.register(UserWallet)
+admin.site.register(ConfirmPayment)
--- intel_invest_app/forms.py
+++ intel_invest_app/forms.py
@@ -1,102 +1,102 @@
-# from django.forms import ModelForm
-# from django import forms
-# from .models import *
-# from django.contrib.auth.forms import UserCreationForm
+from django.forms import ModelForm
+from django import forms
+from .models import *
+from django.contrib.auth.forms import UserCreationForm
 
-# class SignupForm(UserCreationForm):
-#     full_name = forms.CharField(max_length = 200)
-#     email = forms.EmailField()
-#     class Meta:
-#         model = User
-#         fields = ('full_name', 'username', 'email','password1', 'password2')
+class SignupForm(UserCreationForm):
+    full_name = forms.CharField(max_length = 200)
+    email = forms.EmailField()
+    class Meta:
+        model = User
+        fields = ('full_name', 'username', 'email','password1', 'password2')
 
-#     def __init__ (self, *args, **kwargs):
-#         super(SignupForm, self).__init__(*args, **kwargs)
-
-
-#         self.fields['full_name'].widget.attrs['class']= 'form-control'
-#         self.fields['username'].widget.attrs['class']= 'form-control'
-#         self.fields['email'].widget.attrs['class']= 'form-control'
-#         self.fields['password1'].widget.attrs['class']= 'form-control'
-#         self.fields['password2'].widget.attrs['class']= 'form-control'
+    def __init__ (self, *args, **kwargs):
+        super(SignupForm, self).__init__(*args, **kwargs)
 
 
-# class PackagesForm(forms.ModelForm):
-#     class Meta:
-#         model = Packages
-#         fields = ('package_name', 'package_price', 'package_description')
-
-#         widgets={
-#                 'package_name':forms.TextInput(attrs={'class':'form-control', 'placeholder':'Package Name'}),
-#                 'package_price':forms.NumberInput(attrs={'class':'form-control', 'placeholder':'$ Package Price'}),
-#                 'package_description':forms.Textarea(attrs={'class':'form-control',  'placeholder':'Package description...'}),
-#         }
-
-# class UserProfileForm(ModelForm):
-#     class Meta:
-#         model = User
-#         fields = [
-#             'full_name', 
-#             'email', 
-#             'username',
-#         ]
-
-#         widgets={
-#                 'full_name':forms.TextInput(attrs={'class':'form-control', 'placeholder':'Full Name'}),
-#                 'email':forms.EmailInput(attrs={'class':'form-control', 'placeholder':'Email'}),
-#                 'username':forms.TextInput(attrs={'class':'form-control', 'placeholder':'Username'}),
-#         }
-
-# class PaymentForm(ModelForm):
-#     class Meta:
-#         model = Payment
-#         fields = "__all__"
-#         exclude = ['user']
-
-#         widgets={
-#             'transanction_hash':forms.TextInput(attrs={'class':'form-control', 'placeholder':'Payment transanction hash'}),
-#             'screenshot': forms.ImageField(),
-#             'cryptocurrency':forms.Select(choices= Cryptocurrency.objects.all().values_list('name', 'name'), attrs={'class':'form-control'}),
-#             'package':forms.Select(choices= Packages.objects.all().values_list('package_name', 'package_name'), attrs={'class':'form-control'}),
-#     }
-
-#     def __init__(self, *args, **kwargs):
-#         super(PaymentForm, self).__init__(*args, **kwargs)
-
-# class UserWalletForm(ModelForm):
-#     class Meta:
-#         model = UserWallet
-#         fields = "__all__"
-#         exclude = ['user']
-
-#         widgets={
-#             'bitcoin':forms.TextInput(attrs={'class':'form-control', 'placeholder':'Bitcoin wallet'}),
-#             'ethereum':forms.TextInput(attrs={'class':'form-control', 'placeholder':'Ethereum wallet'}),
-#             'litecoin':forms.TextInput(attrs={'class':'form-control', 'placeholder':'Litecoin wallet'}),
-#             'bnb':forms.TextInput(attrs={'class':'form-control', 'placeholder':'Bnb wallet'}),
-#             'busd':forms.TextInput(attrs={'class':'form-control', 'placeholder':'Busd wallet'}),
-#             'usdt':forms.TextInput(attrs={'class':'form-control', 'placeholder':'Usdt wallet'}),
-#     }
-
-#     def user (self, user):
-#         self.user = user.id
-
-#     def __init__(self, *args, **kwargs):
-#         super(UserWalletForm, self).__init__(*args, **kwargs)
+        self.fields['full_name'].widget.attrs['class']= 'form-control'
+        self.fields['username'].widget.attrs['class']= 'form-control'
+        self.fields['email'].widget.attrs['class']= 'form-control'
+        self.fields['password1'].widget.attrs['class']= 'form-control'
+        self.fields['password2'].widget.attrs['class']= 'form-control'
 
 
-# class ConfirmPaymentForm(ModelForm):
-#     class Meta:
-#         model = ConfirmPayment
-#         fields = "__all__"
-#         exlude = ['user']
+class PackagesForm(forms.ModelForm):
+    class Meta:
+        model = Packages
+        fields = ('package_name', 'package_price', 'package_description')
 
-#         widgets={
-#             'reciever_email':forms.EmailInput(attrs={'class':'form-control', 'placeholder':'Reciever Email'}),
-#             'reciever_account':forms.Select(choices= User.objects.all().values_list('email', 'email'), attrs={'class':'form-control'}),
-#             'screenshot': forms.ImageField(),
-#             'body':forms.Textarea(attrs={'class':'form-control', 'placeholder':'Put in a message'}),
-#     }
+        widgets={
+                'package_name':forms.TextInput(attrs={'class':'form-control', 'placeholder':'Package Name'}),
+                'package_price':forms.NumberInput(attrs={'class':'form-control', 'placeholder':'$ Package Price'}),
+                'package_description':forms.Textarea(attrs={'class':'form-control',  'placeholder':'Package description...'}),
+        }
 
-#     def __init__(self, *args, **kwargs):
-#         super(PaymentForm, self).__init__(*args, **kwargs)
+class UserProfileForm(ModelForm):
+    class Meta:
+        model = User
+        fields = [
+            'full_name', 
+            'email', 
+            'username',
+        ]
+
+        widgets={
+                'full_name':forms.TextInput(attrs={'class':'form-control', 'placeholder':'Full Name'}),
+                'email':forms.EmailInput(attrs={'class':'form-control', 'placeholder':'Email'}),
+                'username':forms.TextInput(attrs={'class':'form-control', 'placeholder':'Username'}),
+        }
+
+class PaymentForm(ModelForm):
+    class Meta:
+        model = Payment
+        fields = "__all__"
+        exclude = ['user']
+
+        widgets={
+            'transanction_hash':forms.TextInput(attrs={'class':'form-control', 'placeholder':'Payment transanction hash'}),
+            'screenshot': forms.ImageField(),
+            'cryptocurrency':forms.Select(choices= Cryptocurrency.objects.all().values_list('name', 'name'), attrs={'class':'form-control'}),
+            'package':forms.Select(choices= Packages.objects.all().values_list('package_name', 'package_name'), attrs={'class':'form-control'}),
+    }
+
+    def __init__(self, *args, **kwargs):
+        super(PaymentForm, self).__init__(*args, **kwargs)
+
+class UserWalletForm(ModelForm):
+    class Meta:
+        model = UserWallet
+        fields = "__all__"
+        exclude = ['user']
+
+        widgets={
+            'bitcoin':forms.TextInput(attrs={'class':'form-control', 'placeholder':'Bitcoin wallet'}),
+            'ethereum':forms.TextInput(attrs={'class':'form-control', 'placeholder':'Ethereum wallet'}),
+            'litecoin':forms.TextInput(attrs={'class':'form-control', 'placeholder':'Litecoin wallet'}),
+            'bnb':forms.TextInput(attrs={'class':'form-control', 'placeholder':'Bnb wallet'}),
+            'busd':forms.TextInput(attrs={'class':'form-control', 'placeholder':'Busd wallet'}),
+            'usdt':forms.TextInput(attrs={'class':'form-control', 'placeholder':'Usdt wallet'}),
+    }
+
+    def user (self, user):
+        self.user = user.id
+
+    def __init__(self, *args, **kwargs):
+        super(UserWalletForm, self).__init__(*args, **kwargs)
+
+
+class ConfirmPaymentForm(ModelForm):
+    class Meta:
+        model = ConfirmPayment
+        fields = "__all__"
+        exlude = ['user']
+
+        widgets={
+            'reciever_email':forms.EmailInput(attrs={'class':'form-control', 'placeholder':'Reciever Email'}),
+            'reciever_account':forms.Select(choices= User.objects.all().values_list('email', 'email'), attrs={'class':'form-control'}),
+            'screenshot': forms.ImageField(),
+            'body':forms.Textarea(attrs={'class':'form-control', 'placeholder':'Put in a message'}),
+    }
+
+    def __init__(self, *args, **kwargs):
+        super(PaymentForm, self).__init__(*args, **kwargs)
